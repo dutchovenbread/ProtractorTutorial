88491989d115b1f3c483aadd0d88b31edf6c19aa
Michael Hunter <hunter.michael.k@gmail.com>
2015-01-14 15:12:08 -0500

    Added test for history.

--- spec.js
+++ spec.js
@@ -4,9 +4,30 @@ describe('angularjs homepage', function() {
   var secondNumber = element(by.model('second'));
   var goButton = element(by.id('gobutton'));
   var latestResult = element(by.binding('latest'));
+  var history = element.all(by.repeater('result in memory'));
+  
+  function add(a,b) {
+    firstNumber.sendKeys(a);
+    secondNumber.sendKeys(b);
+    goButton.click();
+  }
   
   beforeEach(function() {
     browser.get('http://juliemr.github.io/protractor-demo/');
+  });
+  
+  it('should have a history', function() {
+    add(1,2);
+    add(3,4);
+    
+    expect(history.count()).toEqual(2);
+    
+    add(5,6);
+    
+    expect(history.count()).toEqual(3);
+    
+    expect(history.last().getText()).toContain('1 + 2');
+    expect(history.first().getText()).toContain('5 + 6x');
   });
   
   it('should have a title', function() {
